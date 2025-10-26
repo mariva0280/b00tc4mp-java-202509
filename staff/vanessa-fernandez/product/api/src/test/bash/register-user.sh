eb6a32cbca515a73c01ce372344d51dcb4bf36d5
mariva0280 <marivafp@gmail.com>
2025-10-26 18:50:54 +0100

    add users servlet to manage users; implement userts registration; #4

---
+++ staff/vanessa-fernandez/product/api/src/test/bash/register-user.sh
@@ -0,0 +1,8 @@
+curl -X POST http://localhost:8080/api/users \
+  -H "Content-Type: application/json" \
+  -d '{
+    "name": "Pepito Grillo",
+    "username": "pepitogrillo",
+    "password": "123123123"
+  }' \
+  -v
